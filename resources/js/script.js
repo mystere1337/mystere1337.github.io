b7ae01dfc2a7585d66657b6b41f0f9711840c3c4
Aymeric Baud <aymeric.baud@epitech.eu>
2021-05-12 13:07:43 +0200

    Add skills section, remake javascript coordinator

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,18 +1,42 @@
+var home = 1;
+
 function hideAllDiv() {
     var a = document.getElementById("contact-container");
+    var b = document.getElementById("skills-container");
     
     a.style.display = "none";
+    b.style.display = "none";
+    home = 1;
 }
 
 function showHome() {
-    document.documentElement.scrollTop = 0;
-    setTimeout(function() { hideAllDiv(); }, 400);
+    if (home === 0) {
+        document.documentElement.scrollTop = 0;
+        setTimeout(function() { hideAllDiv(); }, 500);
+        home = 1;
+    }
+}
+
+function showSkills() {
+    if (home === 1) {
+        var x = document.getElementById("skills-container");
+        x.style.display = "block";
+        window.location.href='#skills-container';
+        home = 0;
+    } else {
+        showHome();
+        setTimeout(function() { showSkills(); }, 500);
+    }
 }
 
 function showContactForm() {
-    var x = document.getElementById("contact-container");
-
-    hideAllDiv();
-
-    x.style.display = "block";
+    if (home === 1) {
+        var x = document.getElementById("contact-container");
+        x.style.display = "block";
+        window.location.href='#contact-container';
+        home = 0;
+    } else {
+        showHome();
+        setTimeout(function() { showContactForm(); }, 500);
+    }
 }
