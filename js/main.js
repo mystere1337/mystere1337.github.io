49f6bddfb299d1d56bf70298729c84711e36c6c5
Aymeric <aymeric.baud@epitech.eu>
2021-12-18 20:33:37 +0100

    Add command line interface, and some commands

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const node = document.getElementById('cmd');
+
+node.addEventListener("keydown", ({ key }) => {
+    if (key === "Enter") {
+        // prints prompt like real terminals
+        print("<b>></b> " + document.getElementById('cmd').value);
+        if (document.getElementById('cmd').value === '') {
+            return;
+        } else if (document.getElementById('cmd').value === 'help') {
+            printHelp();
+        } else if (document.getElementById('cmd').value === 'about') {
+            printAbout();
+        } else {
+            printUnknown();
+        }
+
+        // reset input value to clear command from field
+        document.getElementById('cmd').value = '';
+    }
+})
+
+function print(text, error) {
+    var newDiv = document.createElement("div");
+    var newParagraph = document.createElement("p");
+    error ? newParagraph.classList.add("console-printerror") : newParagraph.classList.add("console-print");
+    newParagraph.innerHTML = text;
+    newDiv.appendChild(newParagraph);
+    var currentDiv = document.getElementById('command-line');
+    var parentDiv = document.getElementById("window-body");
+    parentDiv.insertBefore(newDiv, currentDiv);
+}
+
+function printUnknown() {
+    print("unknown command. try <b>help</b>", true)
+}
+
+function printHelp() {
+    print("help: see all available commands\nabout: information about me\nprojects: projects i'm working on\nclear: clears the console\nsocial: prints my social links", false)
+}
+
+function printAbout() {
+    print("hi, i'm mystere1337, a french computer science student, interested in reverse-engineering, hacking, and windows internals. this is my portfolio.", false)
+}
