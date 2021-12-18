bce8a4ef9cd8abf0d68621ae3099828730ed65bf
Aymeric <aymeric.baud@epitech.eu>
2021-12-18 21:06:03 +0100

    Add projects, Clean code, Add dots

--- js/main.js
+++ js/main.js
@@ -7,13 +7,15 @@ node.addEventListener("keydown", ({ key }) => {
         if (document.getElementById('cmd').value === '') {
             return;
         } else if (document.getElementById('cmd').value === 'help') {
-            printHelp();
+            print("help: see all available commands\nabout: information about me\nprojects: projects i'm working on\nclear: clears the console\nsocial: prints my social links", false)
         } else if (document.getElementById('cmd').value === 'about') {
-            printAbout();
+            print("hi, i'm mystere1337, a french computer science student, interested in reverse-engineering, hacking, and windows internals. this is my portfolio.", false)
+        } else if (document.getElementById('cmd').value === 'projects') {
+            print("stealth: a stealthy game hacking solution.\nmystere1337.github.io: this portfolio\nvaulth: an app using NFTs to virtually authenticate physical objects");
         } else if (document.getElementById('cmd').value === 'clear') {
             clear();
         } else {
-            printUnknown();
+            print("unknown command. try <b>help</b>", true)
         }
 
         // reset input value to clear command from field
@@ -30,18 +32,6 @@ function print(text, error) {
     var currentDiv = document.getElementById('command-line');
     var parentDiv = document.getElementById("window-body");
     parentDiv.insertBefore(newDiv, currentDiv);
-}
-
-function printUnknown() {
-    print("unknown command. try <b>help</b>", true)
-}
-
-function printHelp() {
-    print("help: see all available commands\nabout: information about me\nprojects: projects i'm working on\nclear: clears the console\nsocial: prints my social links", false)
-}
-
-function printAbout() {
-    print("hi, i'm mystere1337, a french computer science student, interested in reverse-engineering, hacking, and windows internals. this is my portfolio.", false)
 }
 
 function clear() {
