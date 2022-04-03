e1d2a4ea7a7251eace25179057e056bdeb19687b
Aymeric Baud <aymeric.baud@epitech.eu>
2022-04-03 15:35:45 +0200

    v3.0 Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Article from "./components/Article.svelte";
+</script>
+
+<main>
+	<Article
+		image="https://github.com/mystere1337.png"
+		title="Aymeric Baud"
+		description="French computer science student. Passionate about computing
+	and IT, always in search to discover new technologies and
+	trying to understand how they work. Welcome to my portfolio,
+	written in Svelte."
+	/>
+</main>
+
+<style>
+	main {
+		max-width: 1200px;
+		margin: auto;
+	}
+</style>
---
+++ src/components/Article.svelte
@@ -0,0 +1,83 @@
+<script>
+    import SvgLink from "./SvgLink.svelte";
+
+    export let image;
+    export let title;
+    export let description;
+</script>
+
+<div class="container">
+    <div class="flex-container">
+        <div class="image-container">
+            <img src={image} alt="" />
+        </div>
+        <div class="article">
+            <h1>{title}</h1>
+            <br />
+            <span class="subtitle">{description}</span>
+            <br />
+            <div class="links-container">
+                <SvgLink
+                    icon={"github"}
+                    link={"https://github.com/mystere1337/"}
+                />
+                <SvgLink
+                    icon={"twitter"}
+                    link={"https://twitter.com/mystere1337/"}
+                />
+                <SvgLink
+                    icon={"linkedin"}
+                    link={"https://www.linkedin.com/in/aymeric-baud/"}
+                />
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .container {
+        padding: 60px 0 60px 0;
+    }
+    .flex-container {
+        display: flex;
+        gap: 50px;
+    }
+    .links-container {
+        display: flex;
+        gap: 20px;
+    }
+    .image-container > img {
+        flex-shrink: 1;
+        min-width: 100%;
+        min-height: 100%;
+    }
+    .image-container {
+        background-color: blue;
+        border-radius: 500px;
+        min-width: 256px;
+        min-height: 256px;
+        max-width: 256px;
+        max-height: 256px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+    }
+
+    .article {
+        max-width: 590px;
+        display: flex;
+        flex-direction: column;
+    }
+    h1 {
+        font-size: 64px;
+        font-weight: bold;
+        margin: 0;
+    }
+    .subtitle {
+        font-size: 20px;
+    }
+    .subtitle {
+        font-size: 20px;
+    }
+</style>
